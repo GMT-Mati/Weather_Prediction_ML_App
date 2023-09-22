a7c8d75284b956cd13a94371fad3f34555d6ca60
Mateusz Gruszka <gruszka.mateusz87@gmail.com>
2023-09-22 12:54:38 +0200

    1.0.7

--- weather_app/model_retraining.py
+++ weather_app/model_retraining.py
@@ -9,7 +9,7 @@ import joblib
 from weather_app.models import WeatherData  # Import your WeatherData model
 
 class Command(BaseCommand):
-    help = 'Retrain the machine learning model with combined data'
+    help = 'Retrain the machine learning model with combined data and deploy it'
 
     def handle(self, *args, **kwargs):
         # Load historical data from the database
@@ -45,10 +45,21 @@ class Command(BaseCommand):
         test_score = model.score(X_test, y_test)
         print(f'Model Test Score: {test_score}')
 
-        # Save the trained model
+        # Define the directory to store the model
         model_dir = "models"  # Replace with the path to your models directory
         os.makedirs(model_dir, exist_ok=True)
+
+        # Define the model filename
         model_filename = os.path.join(model_dir, "weather_prediction_model.pkl")
+
+        # Delete the existing model file (if it exists)
+        if os.path.exists(model_filename):
+            os.remove(model_filename)
+
+        # Save the trained model
         joblib.dump(model, model_filename)
 
         self.stdout.write(self.style.SUCCESS('Model retraining and update completed'))
+
+        # Perform model deployment here (e.g., restart the web server or update a model reference)
+
